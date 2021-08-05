7fc8f609a5bb7f6207a9ffb117e62ea7a8636788
Alex Rae <arae@spartaglobal.com>
2021-08-05 09:02:30 +0100

    Finished Dice

--- Methods_Lab_Starter/Methods_Tests/DiceTests.cs
+++ Methods_Lab_Starter/Methods_Tests/DiceTests.cs
@@ -6,6 +6,14 @@ namespace Methods_Tests
 {
     public class DiceTests
     {
+        [TestCase(86, 10)]
+        [TestCase(140, 7)]
+        public void DiceReturn_TheSum_FromRandom(int seed, int expResult)
+        {
+            var rng = new Random(seed);
+            var result = Methods.RollDice(rng);
 
+            Assert.That(result, Is.EqualTo(expResult));
+        }
     }
 }
